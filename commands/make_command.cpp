fee192ae0a0b15b37928e16ffbaf9640c676ecf7
Eden Reich <eden.reich@gmail.com>
2019-10-19 04:37:26 +0200

    instead of search and replace simply create string with the given name

--- commands/make_command.cpp
+++ commands/make_command.cpp
@@ -47,10 +47,60 @@ ExitCode MakeCommand::handle(Interfaces::InputInterface * input, Interfaces::Out
         return ExitCode::NeedHelp;
     }
 
+    std::string name = input->getOption("name");
     // @todo write the implemention
+    
+    std::string cppFile =
+"#include \"xxx_command.h\"\n\n\n"
+"class " + name + " {\n\n"
+"/**\n"
+" * Retrieve the name of the command.\n"
+" *\n"
+" * @return std::string\n"
+" */\n"
+"std::string " + name + "::getName()\n"
+"{\n"
+"    return \"default\";\n"
+"}\n\n"
+"/**\n"
+" * Retrieve the description of the command.\n"
+" *\n"
+" * @return std::string\n"
+" */\n"
+"std::string " + name + "::getDescription()\n"
+"{\n"
+"    return \"Default Description\";\n"
+"}\n\n"
+"/**\n"
+" * Retrieve the command options.\n"
+" *\n"
+" * @return Types::AvailableOptions\n"
+" */\n"
+"Types::AvailableOptions " + name + "::getOptions()\n"
+"{\n"
+"    Types::AvailableOptions options;\n"
+"    \n"
+"    return options\n"
+"}\n\n"
+"/**\n"
+" * Handle the command.\n"
+" *\n"
+" * @param InputInterface * input\n"
+" * @param OutputInterface * output\n"
+" * @return ExitCode\n"
+" */\n"
+"ExitCode " + name + "::handle(Interfaces::InputInterface * input, Interfaces::OutputInterface * output)\n"
+"{\n"
+"    if (input->wantsHelp()) {\n"
+"        output->printCommandHelp(this);\n"
+"        return ExitCode::NeedHelp;\n"
+"    }\n\n"
+"    \\\\ Implemention goes here.. \n\n"
+"    return ExitCode::Ok;\n"
+"}\n";
 
-    // create a macro that contains a command strcture and one that contains a header structure that have placeholders (unique identifiers / tokens).
-    // perform search and replace on this tokens
+
+    output->info(cppFile);
     // create a cpp file and header file in commands directory with the rendered output.
 
     return ExitCode::Ok;
